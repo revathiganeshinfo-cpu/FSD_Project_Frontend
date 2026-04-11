da892816d130c77c2cad74810886e0ae67df92e9
revathiganeshinfo-cpu <revathiganeshinfo@gmail.com>
2026-04-11 16:33:25 +0530

    connected backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://fsd-project-backend-n2dp.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
